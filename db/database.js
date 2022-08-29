a858dcd1a0eeeb0b4eae0d9222f83ee776ff44c1
2020-byte <gmmoon0824@gmail.com>
2022-08-29 11:00:58 +0900

    postgres

--- db/database.js
+++ db/database.js
@@ -2,9 +2,17 @@ import { config } from "../config.js";
 import SQ from 'sequelize';
 
 
-const {host, user, database, password} = config.db;
+const {host, port, user, database, password} = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
     host,
-    dialect: 'mysql',
+    port,
+    dialect: 'postgres',
+    logging: false,
     //logging: false, 데이터베이스 터미널에 로그남는 거 꺼줌.
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
